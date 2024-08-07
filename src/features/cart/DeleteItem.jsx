19ddebc6b5fd1b70738ce858425614a6f5f20bb1
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-08-07 16:17:39 +0800

    add components and features

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,15 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button'
+
+function DeleteItem(pizzaId) {
+    const dispatch= useDispatch();
+
+    function handleDelete() {
+        dispatch(deleteItem(pizzaId))
+      }
+  return (
+    <Button type="small" onClick={handleDelete}>Delete</Button>
+  )
+}
+
+export default DeleteItem
